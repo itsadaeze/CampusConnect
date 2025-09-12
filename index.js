08568c95e9200c63acf69f9b4685a9182bb7496e
itsadaeze <adaezeugwu6@gmail.com>
2025-09-12 15:30:10 +0100

    worked on the home page

--- index.js
+++ index.js
@@ -24,3 +24,54 @@ function navbarToggle() {
     toggleIcon.classList.toggle("open");
   });
 }
+
+
+
+
+// Slider-section
+const slidesWrapper = document.querySelector(".slides-section");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+const sliderImages = [
+  "assets/images/homepage/slider1.jpg",
+   "assets/images/homepage/slider2.jpg",
+    "assets/images/homepage/slider3.jpg",
+];
+
+let currentSlide = 0;
+
+
+function slider() {
+  slidesWrapper.innerHTML = sliderImages.map(
+    (img) => `<img src="${img}" alt="slide-banner">`
+  ).join("");
+}
+slider();
+
+function showSlide(index) {
+  currentSlide = (index + sliderImages.length) % sliderImages.length;
+  slidesWrapper.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => showSlide(currentSlide + 1));
+prevBtn.addEventListener("click", () => showSlide(currentSlide - 1));
+setInterval(() => showSlide(currentSlide + 1), 5000);
+
+
+
+// upcoming events
+fetch("data/event.json")
+  .then(res => res.json())
+  .then(data => {
+
+    const eventCards = document.getElementById("event-section-cards");
+    eventCards.innerHTML = data.map(event => `
+      <div class="event-section-card" onclick="window.location.href='event.html'">
+        <img src="${event.image}" alt="${event.title}" style="width:100%; border-radius:8px; margin-bottom:0.5rem;">
+        <h3>${event.title}</h3>
+        <p><strong>Date:</strong> ${event.date}</p>
+        <p>${event.description}</p>
+      </div>
+    `).join("");
+  });
